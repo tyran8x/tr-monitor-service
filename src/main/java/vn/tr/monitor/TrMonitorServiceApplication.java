78a0c7d10b192a9c3e08313556b2d662fd9c3626
tyran8x <tyran8x@gmail.com>
2025-08-05 23:50:04 +0700

    update

--- src/main/java/vn/tr/monitor/TrMonitorServiceApplication.java
+++ src/main/java/vn/tr/monitor/TrMonitorServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MonitorServiceApplication {
+public class TrMonitorServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MonitorServiceApplication.class, args);
+		SpringApplication.run(TrMonitorServiceApplication.class, args);
 	}
 
 }
